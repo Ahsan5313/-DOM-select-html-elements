28f0d04d58bcfd078d34d6da2bd72bd20ddf7050
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-09 17:28:51 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName( "para1" )[0].innerHTML = "key";
+
+document.getElementsByClassName( "para2" )[0].innerHTML = "miss";
+
+
+document.getElementsByClassName("sir")[0].innerHTML = "sir";
+document.getElementsByClassName("sir")[1].innerHTML = "sir";
+document.getElementsByClassName("sir")[2].innerHTML = "sir";
